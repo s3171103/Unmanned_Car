de654a912a4430e94b46799c862dcc27ea328a19
Uhong Lin <yh03172012@gmail.com>
2018-06-30 01:31:48 +0800

    Adjust camera and UI

--- Camera_pyqt.py
+++ Camera_pyqt.py
@@ -8,6 +8,8 @@ from PyQt5.QtWidgets import *
 import os
 from camera import *
 
+ser=serial.Serial("/dev/ttyACM0",115200)
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -16,6 +18,25 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.image=None
         self.startButton.clicked.connect(self.start_webcam)
         self.stopButton.clicked.connect(self.stop_webcam)
+
+        self.horizontalSlider.valueChanged.connect(self.setH)
+        self.verticalSlider.valueChanged.connect(self.setV)
+
+    def setH(self):
+        h=self.horizontalSlider.value()
+        strh='H' + str(h) + '\n'
+        print(strh)
+        ser.write(strh.encode())
+        time.sleep(0.06)
+    def setV(self):
+        v=self.verticalSlider.value()
+        strv='v' + str(v) + '\n'
+        ser.write(strv.encode())
+        #ser.write(b'v+')
+        time.sleep(0.06)
+        if a==0:
+            camera.stop_preview()
+            a=1
 
 
     def start_webcam(self):
